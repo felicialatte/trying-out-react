ff3f220b11c072d49ae0687936ba0e2eed9bbe5e
Felicia Audrey <audreyfelicia.feli@gmail.com>
2025-03-16 15:14:32 +0100

    Added axios integration to show the city's weather

---
+++ src/Weather.js
@@ -0,0 +1,26 @@
+import React from "react";
+import axios from "axios"
+import { FidgetSpinner } from 'react-loader-spinner'
+
+
+export default function Weather(props) {
+    function handleResponse(response) {
+        alert(`The weather in ${response.data.name} is ${response.data.main.temp}C`)
+    }
+    
+    const apiKey = `701f06352d61835bc4fc894e7b084629`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+    
+    axios.get(apiUrl).then(handleResponse);
+    
+    return (
+        <FidgetSpinner
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="fidget-spinner-loading"
+            wrapperStyle={{}}
+            wrapperClass="fidget-spinner-wrapper"
+        />
+    )
+}
